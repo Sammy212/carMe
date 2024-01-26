78cc1967d163e388203ba3a6e477817d5a6d61dc
Sammy212 <samuelafo.212@gmail.com>
2024-01-26 15:01:17 +0100

    update App.js

--- App.js
+++ App.js
@@ -2,13 +2,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+//Pages
 import HomeScreen from "./src/HomeScreen";
 import MapScreen from "./src/MapScreen";
 import SavedScreen from "./src/SavedScreen";
 import SettingsScreen from "./src/SettingsScreen";
 import InfoScreen from "./src/InfoScreen";
+
+// Assets
+const homeIcon_active = require("./src/assets/icons/home-active.png");
+const homeIcon = require("./src/assets/icons/home.png");
+const compass_active = require("./src/assets/icons/compass-active.png");
+const compass = require("./src/assets/icons/compass.png");
+const savedIcon_active = require("./src/assets/icons/saved-active.png");
+const savedIcon = require("./src/assets/icons/saved.png");
+const settingsIcon_active = require("./src/assets/icons/settings-active.png");
+const settingsIcon = require("./src/assets/icons/settings.png");
 
 
 const Stack = createNativeStackNavigator();
@@ -31,7 +42,38 @@ function HomeStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? homeIcon_active : homeIcon;
+            } else if (route.name === "Map") {
+              iconName = focused ? compass_active : compass;
+            } else if (route.name === "Saved") {
+              iconName = focused ? savedIcon_active : savedIcon;
+            } else if (route.name === "Settings") {
+              iconName = focused ? settingsIcon_active : settingsIcon;
+            }
+
+            return (
+              <Image source={iconName} resizeMode="contain" style={styles.footerIcon} />
+            )
+          },
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: "absolute",
+            padding: 10,
+            // height: 60,
+            // paddingBottom: 30,
+            backgroundColor: 'black',
+            borderTopLeftRadius: 25,
+            borderTopRightRadius: 25,
+          }
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Saved" component={SavedScreen} />
@@ -48,4 +90,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  footerIcon: {
+    width: 20
+  }
 });
